b67eb9611c404da5b216c5a508421b2684e4ae05
Vishwam1158 <vishwam1158@gmail.com>
2024-06-19 16:06:14 +0530

    update MainActivity

--- app/src/main/java/com/viz/to_do_listapp/MainActivity.kt
+++ app/src/main/java/com/viz/to_do_listapp/MainActivity.kt
@@ -4,44 +4,50 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.room.Room
+import com.viz.to_do_listapp.roomDB.TaskDatabase
 import com.viz.to_do_listapp.ui.theme.ToDOlistAppTheme
+import com.viz.to_do_listapp.viewModel.Repository
+import com.viz.to_do_listapp.viewModel.TaskViewModel
 
 class MainActivity : ComponentActivity() {
+    private val db by lazy {
+        Room.databaseBuilder(
+            applicationContext,
+            TaskDatabase::class.java,
+            "todolist.db"
+        ).build()
+    }
+    private val viewModel by viewModels<TaskViewModel> ( // In the viewModel we pass repository so here we use factory producer
+        factoryProducer = {
+            object : ViewModelProvider.Factory {
+                override fun <T : ViewModel> create(modelClass: Class<T>): T {
+                    return TaskViewModel(Repository(db)) as T
+                }
+            }
+        }
+    )
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             ToDOlistAppTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Navigation(viewModel)
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ToDOlistAppTheme {
-        Greeting("Android")
-    }
-}
