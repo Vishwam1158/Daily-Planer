ec4f663ec1fcf55fd00bba5000c29e605ce61b2d
Vishwam1158 <vishwam1158@gmail.com>
2024-06-22 23:23:56 +0530

    Add Animated Bottom Bar

---
+++ app/src/main/java/com/viz/to_do_listapp/animation/AnimationScreen.kt
@@ -0,0 +1,137 @@
+package com.viz.to_do_listapp.animation
+
+
+import android.graphics.ColorMatrix
+import android.graphics.ColorMatrixColorFilter
+import android.graphics.RenderEffect
+import android.graphics.Shader
+import android.os.Build
+import androidx.annotation.RequiresApi
+import androidx.compose.animation.core.LinearEasing
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.asComposeRenderEffect
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+
+@RequiresApi(Build.VERSION_CODES.S)
+private fun getRenderEffect(): RenderEffect {
+    val blurEffect = RenderEffect
+        .createBlurEffect(80f, 80f, Shader.TileMode.MIRROR)
+
+    val alphaMatrix = RenderEffect.createColorFilterEffect(
+        ColorMatrixColorFilter(
+            ColorMatrix(
+                floatArrayOf(
+                    1f, 0f, 0f, 0f, 0f,
+                    0f, 1f, 0f, 0f, 0f,
+                    0f, 0f, 1f, 0f, 0f,
+                    0f, 0f, 0f, 50f, -5000f
+                )
+            )
+        )
+    )
+
+    return RenderEffect
+        .createChainEffect(alphaMatrix, blurEffect)
+}
+
+//Animation Effect
+@Composable
+fun AnimationScreen(
+//    selectedRoute : String = Routes.Home.route
+) {
+    val isMenuExtended = remember { mutableStateOf(false) }
+
+    val fabAnimationProgress by animateFloatAsState(
+        targetValue = if (isMenuExtended.value) 1f else 0f,
+        animationSpec = tween(
+            durationMillis = 1000,
+            easing = LinearEasing
+        )
+    )
+
+    val clickAnimationProgress by animateFloatAsState(
+        targetValue = if (isMenuExtended.value) 1f else 0f,
+        animationSpec = tween(
+            durationMillis = 400,
+            easing = LinearEasing
+        )
+    )
+
+    val renderEffect = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+        getRenderEffect().asComposeRenderEffect()
+    } else {
+        null
+    }
+        AnimationScreen(
+            renderEffect = renderEffect,
+            fabAnimationProgress = fabAnimationProgress,
+            clickAnimationProgress = clickAnimationProgress,
+        ) {
+            isMenuExtended.value = isMenuExtended.value.not()  // Toggle the FloatActionButton  and Provide Option
+        }
+}
+
+
+@Composable
+fun AnimationScreen(
+    renderEffect: androidx.compose.ui.graphics.RenderEffect?,
+    fabAnimationProgress: Float = 0f,
+    clickAnimationProgress: Float = 0f,
+    toggleAnimation: () -> Unit = { },
+) {
+    Box(
+        Modifier
+            .fillMaxSize()
+            .padding(bottom = 24.dp),
+        contentAlignment = Alignment.BottomCenter
+    ) {
+//        BottomAppBar()
+
+        FabGroup(renderEffect = renderEffect, animationProgress = fabAnimationProgress)
+        FabGroup(
+            renderEffect = null,
+            animationProgress = fabAnimationProgress,
+            toggleAnimation = toggleAnimation
+        )
+
+        Circle(
+            color = MaterialTheme.colorScheme.tertiary.copy(alpha = 0.5f),
+            animationProgress = 0.5f
+        )
+
+        FabGroup(renderEffect = renderEffect, animationProgress = fabAnimationProgress)
+        FabGroup(
+            renderEffect = null,
+            animationProgress = fabAnimationProgress,
+            toggleAnimation = toggleAnimation
+        )
+
+        Circle(
+            color = Color.White,
+            animationProgress = clickAnimationProgress
+        )
+    }
+}
+
+
+
+
+@Preview(showBackground = true)
+@Composable
+private fun AnimationScreennPreview() {
+    AnimationScreen(null)
+}
