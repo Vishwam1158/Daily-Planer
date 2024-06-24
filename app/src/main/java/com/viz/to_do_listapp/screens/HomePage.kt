6aca03dd64cd360b1815e27e390db2bb7e98d6bd
Vishwam1158 <vishwam1158@gmail.com>
2024-06-24 22:15:36 +0530

    Add Task Item

--- app/src/main/java/com/viz/to_do_listapp/screens/HomePage.kt
+++ app/src/main/java/com/viz/to_do_listapp/screens/HomePage.kt
@@ -1,38 +1,43 @@
 package com.viz.to_do_listapp.screens
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material3.Button
-import androidx.compose.material3.Divider
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import com.viz.to_do_listapp.roomDB.Task
 import com.viz.to_do_listapp.viewModel.TaskViewModel
 import androidx.compose.ui.platform.LocalLifecycleOwner
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
+import com.viz.to_do_listapp.R
 
 
 @Composable
 fun HomePage( viewModel: TaskViewModel, navController: NavController) {
-    var title by remember { mutableStateOf(" ") }
-    var description by remember { mutableStateOf(" ") }
-    val task = Task(title, description)
     var taskList by remember { mutableStateOf(listOf<Task>()) }
     viewModel.getTasks().observe(LocalLifecycleOwner.current) { taskList = it }
+
+    var showOverlayState by remember { mutableStateOf(mapOf<Int, Boolean>()) }
 
 //    val gradient = Brush.verticalGradient(
 //        colors = listOf( Color(0xFF222222),Color(0xFFD7D2C9),)
@@ -42,39 +47,98 @@ fun HomePage( viewModel: TaskViewModel, navController: NavController) {
         modifier = Modifier.padding(16.dp),//.background(gradient),
         verticalArrangement = Arrangement.spacedBy(12.dp)
     ) {
-        Button(onClick = {
-            viewModel.upsertTask(task)
-        }) {
-            Text(text = "set data")
-        }
-
-        TextField(
-            value = title,
-            onValueChange = { title = it },
-            placeholder = { Text(text = "Title") }
-        )
-
-        TextField(
-            value = description,
-            onValueChange = { description = it },
-            placeholder = { Text(text = "description") }
-        )
 
         LazyColumn {
             items(taskList) { task ->
+                val isOverlayShown = showOverlayState[task.id] ?: false
+
                 Column(
                     Modifier.clickable { viewModel.deleteTask(task) }
                 ) {
-                    Text(text = "Task: ${task.title}")
-                    Spacer(modifier = Modifier.height(8.dp))
-                    Text(text = "Description: ${task.description}")
-                    Divider(
-                        Modifier
-                            .fillMaxWidth()
-                            .padding(8.dp))
+                    Box(
+                        contentAlignment = Alignment.Center,
+                    ) {
+                        Image(painter = painterResource(id = R.drawable.task_title_border ), contentDescription = "")
+
+                        IconButton(onClick = {
+                            showOverlayState = showOverlayState.toMutableMap().apply {
+                                this[task.id] = !isOverlayShown }
+                        },
+                            modifier = Modifier
+                                .align(Alignment.TopStart)
+                                .padding( start = 28.dp, top = 4.dp)
+                                .size(64.dp),
+                        ) {
+                            Icon(
+                                painter = painterResource(id = R.drawable.task_round),
+                                contentDescription = "Example Image",
+                                modifier = Modifier.size(48.dp),
+                                tint = Color.Black
+                            )
+                        }
+                        if (isOverlayShown) {//showOverlay
+                            Image(
+                                painter = (painterResource(R.drawable.task_tick)),
+                                contentDescription = null,
+                                modifier = Modifier
+                                    .align(Alignment.TopStart).padding(start = 36.dp)
+                                    .size(56.dp)
+                            )
+                        }
+
+                        Text(
+                            text = task.title,
+                            fontSize = 24.sp,
+                            fontWeight = FontWeight.Bold,
+                            modifier = Modifier.align(Alignment.CenterStart).padding(start = 108.dp)
+                        )
+
+
+                    }
                 }
             }
         }
     }
 }
 
+@Preview(showBackground = true)
+@Composable
+private fun TempPreview() {
+//    Box(contentAlignment = Alignment.Center) {
+//
+//        Box(
+//            contentAlignment = Alignment.Center,
+//        ) {
+//            Image(painter = painterResource(id = R.drawable.task_title_border ), contentDescription = "")
+//
+//            IconButton(onClick = {  },
+//                modifier = Modifier
+//                    .align(Alignment.TopStart)
+//                    .padding( start = 28.dp, top = 4.dp).size(64.dp),
+//            ) {
+//                Icon(
+//                    painter = painterResource(id = R.drawable.task_round),
+//                    contentDescription = "Example Image",
+//                    modifier = Modifier.size(48.dp),
+//                    tint = Color.Black
+//                )
+//            }
+//            Image(
+//                painter = (painterResource(R.drawable.task_tick)),
+//                contentDescription = null,
+//                modifier = Modifier
+//                    .align(Alignment.TopStart).padding(start = 36.dp)
+//                    .size(56.dp)
+//            )
+//
+//
+//
+//            }
+//            Text(
+//                text = "vizzzzzzzzzzzzzzz",
+//                fontSize = 24.sp,
+//                fontWeight = FontWeight.Bold,
+//                modifier = Modifier.align(Alignment.CenterStart).padding(start = 108.dp)
+//            )
+//    }
+}
