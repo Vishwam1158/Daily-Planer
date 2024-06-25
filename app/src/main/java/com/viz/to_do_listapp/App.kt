d03171fbfa9a669c98f0539144701fa9b0de26cc
Vishwam1158 <vishwam1158@gmail.com>
2024-06-25 22:44:46 +0530

    change BottomAppBar to content and animation

--- app/src/main/java/com/viz/to_do_listapp/App.kt
+++ app/src/main/java/com/viz/to_do_listapp/App.kt
@@ -1,14 +1,19 @@
 package com.viz.to_do_listapp
 
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
-import com.viz.to_do_listapp.screens.BottomAppBar
+import com.viz.to_do_listapp.animation.AnimationScreen
+import com.viz.to_do_listapp.animation.BottomAppBar
 import com.viz.to_do_listapp.screens.Calendar
 import com.viz.to_do_listapp.screens.HomePage
 import com.viz.to_do_listapp.screens.TopAppBar
@@ -20,23 +25,43 @@ fun App(viewModel : TaskViewModel, navController: NavController) {
 
     Scaffold(
         topBar = {
-                 TopAppBar()
+            TopAppBar()
         },
-        bottomBar = {
-            BottomAppBar(selectedRoute = selectedRoute.value,
-                onChange = { route ->
-                    selectedRoute.value = route
-                    selectedRoute.value = route
-                })
-        }
+//        bottomBar = {
+//            BottomAppBar(
+//                selectedRoute = selectedRoute.value,
+//                onChange = { route ->
+//                    selectedRoute.value = route
+//                }
+//            )
+//        },
+//        content = {
+//            Box(modifier = Modifier.padding(it)) {}
+//        }
     ) {
         Column(modifier = Modifier.padding(it)) {
-            when(selectedRoute.value) {
+            when (selectedRoute.value) {
                 Routes.Home.route -> HomePage(viewModel, navController)
                 Routes.Calendar.route -> Calendar(navController)
 
             }
         }
+    }
+
+    Box(
+        Modifier
+            .fillMaxSize()
+            .padding(bottom = 16.dp), //24.dp
+        contentAlignment = Alignment.BottomCenter
+    ) {
+
+            BottomAppBar(
+                selectedRoute = selectedRoute.value,
+                onChange = { route ->
+                    selectedRoute.value = route
+                }
+            )
+            AnimationScreen(navController = navController, viewModel)
     }
 }
 
