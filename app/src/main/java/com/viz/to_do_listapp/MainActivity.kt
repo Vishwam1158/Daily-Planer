28f267854359d1af7f9a0dad09f43e1951e33973
Vishwam1158 <vishwam1158@gmail.com>
2024-06-27 23:23:12 +0530

    add newRelic and toggle theme button

--- app/src/main/java/com/viz/to_do_listapp/MainActivity.kt
+++ app/src/main/java/com/viz/to_do_listapp/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.viz.to_do_listapp
 
 import android.os.Bundle
+import com.newrelic.agent.android.NewRelic
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -8,6 +9,10 @@ import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
@@ -22,10 +27,10 @@ class MainActivity : ComponentActivity() {
         Room.databaseBuilder(
             applicationContext,
             TaskDatabase::class.java,
-            "todolist.db"
+            "todolist2.db"
         ).build()
     }
-    private val viewModel by viewModels<TaskViewModel> ( // In the viewModel we pass repository so here we use factory producer
+    private val viewModel by viewModels<TaskViewModel>( // In the viewModel we pass repository so here we use factory producer
         factoryProducer = {
             object : ViewModelProvider.Factory {
                 override fun <T : ViewModel> create(modelClass: Class<T>): T {
@@ -34,18 +39,26 @@ class MainActivity : ComponentActivity() {
             }
         }
     )
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        NewRelic.withApplicationToken(
+            "AAd89327b71b784e1d20f265c8562218d9226a5f74-NRMA"
+        ).start(this.applicationContext)
+
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            ToDOlistAppTheme {
+
+            var darkTheme by remember { mutableStateOf(false) }
+            ToDOlistAppTheme(darkTheme) {
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Navigation(viewModel)
+                    Navigation(viewModel = viewModel, darkTheme = darkTheme, onThemeUpdated = { darkTheme = !darkTheme })
                 }
             }
+
         }
     }
 }
