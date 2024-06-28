6711cc852564364f69b5362597ccdf68323df6cf
Vishwam1158 <vishwam1158@gmail.com>
2024-06-28 21:25:25 +0530

    add color for Light/Dark theme

--- app/src/main/java/com/viz/to_do_listapp/ui/theme/Color.kt
+++ app/src/main/java/com/viz/to_do_listapp/ui/theme/Color.kt
@@ -2,13 +2,22 @@ package com.viz.to_do_listapp.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Pink80 = Color(0xFFEFB8C8)
+val DarkPrimaryText = Color(0xFFFFFFFF)
+val DarkContainerColor = Color(0xFFFFFFFF)
+val DarkBackground = Color(0xFF1B1B1B)
+val DarkPrimaryTint = Color(0xFFFFFFFF)
+val DarkSecondaryTint = Color(0xFFBFAFB2)
+val DarkTertiaryTint = Color(0xFF000000)
 
-val Pink40 = Color(0xFF7D5260)
 
 
-val PrimaryText = Color(0xFFFFFFFF)
-val SecondaryText = Color(0xFF000000)
 
-val PrimaryTint = Color(0xFF000000)
-val SecondaryTint = Color(0xFF808080)
+val LightPrimaryText = Color(0xFF1B1B1B)
+val LightContainerColor = Color(0xFFFFFFFF)
+val LightBackground = Color(0xFFFFFFFF)
+val LightPrimaryTint = Color(0xFF000000)
+val LightSecondaryTint = Color(0xFF808080)
+val LightTertiaryTint = Color(0xFFFFFFFF)
+
+
+// Black  - 1A2421, 1B1B1B
