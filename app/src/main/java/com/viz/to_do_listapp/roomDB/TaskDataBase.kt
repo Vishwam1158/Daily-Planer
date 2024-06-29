5ed97dee5888b49a82140a584b2ac99ebe155ce2
Vishwam1158 <vishwam1158@gmail.com>
2024-06-29 23:19:03 +0530

    update database

--- app/src/main/java/com/viz/to_do_listapp/roomDB/TaskDataBase.kt
+++ app/src/main/java/com/viz/to_do_listapp/roomDB/TaskDataBase.kt
@@ -1,9 +1,45 @@
 package com.viz.to_do_listapp.roomDB
 
+import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
+import androidx.room.migration.Migration
+import androidx.sqlite.db.SupportSQLiteDatabase
+
 
 @Database(entities = [Task::class], version = 1)
 abstract class TaskDatabase: RoomDatabase() {
-    abstract val dao : RoomDao
+    abstract val dao: RoomDao
+
+//    companion object {
+//        @Volatile
+//        private var INSTANCE: TaskDatabase? = null
+
+//        fun getDatabase(context: Context): TaskDatabase {
+//            return INSTANCE ?: synchronized(this) {
+//                val instance = Room.databaseBuilder(
+//                    context.applicationContext,
+//                    TaskDatabase::class.java,
+//                    "task_database"
+//                )
+//                    .addMigrations(MIGRATION_1_2)
+//                    .build()
+//                INSTANCE = instance
+//                instance
+//            }
+//        }
+
+//        private val MIGRATION_1_2 = object : Migration(1, 2) {
+//            override fun migrate(database: SupportSQLiteDatabase) {
+//                database.execSQL("ALTER TABLE Task ADD COLUMN isComplete INTEGER NOT NULL DEFAULT 0")
+//            }
+//        }
+//    }
 }
+
+
+//@Database(entities = [Task::class], version = 1)
+//abstract class TaskDataBase: RoomDatabase() {
+//    abstract val doa : RoomDao
+//}
