22759fb24792472ac4690d6ab06733665a7036bd
Vishwam1158 <vishwam1158@gmail.com>
2024-07-01 23:07:55 +0530

    add Category

--- app/src/main/java/com/viz/to_do_listapp/viewModel/Repository.kt
+++ app/src/main/java/com/viz/to_do_listapp/viewModel/Repository.kt
@@ -1,5 +1,6 @@
 package com.viz.to_do_listapp.viewModel
 
+import com.viz.to_do_listapp.roomDB.Category
 import com.viz.to_do_listapp.roomDB.Task
 import com.viz.to_do_listapp.roomDB.TaskDatabase
 
@@ -12,7 +13,16 @@ class Repository(private val db : TaskDatabase) {
         db.dao.deleteTask(task)
     }
 
-//    suspend fun getTaskById(id: Int) = db.dao.getTaskById(id)
-
     fun getAllTasks() = db.dao.getAllTasks()
+
+    // for category
+    suspend fun upsertCategory(category: Category) {
+        db.dao.upsertCategory(category)
+    }
+
+    suspend fun deleteCategory(category: Category) {
+        db.dao.deleteCategory(category)
+    }
+
+    fun getAllCategories() = db.dao.getAllCategories()
 }
