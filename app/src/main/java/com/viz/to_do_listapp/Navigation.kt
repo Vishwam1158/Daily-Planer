1c09e0bd46a7e18a244033af06091d5744a78d21
Vishwam1158 <vishwam1158@gmail.com>
2024-07-05 22:46:57 +0530

    add category in navigation

--- app/src/main/java/com/viz/to_do_listapp/Navigation.kt
+++ app/src/main/java/com/viz/to_do_listapp/Navigation.kt
@@ -1,33 +1,45 @@
 package com.viz.to_do_listapp
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.viz.to_do_listapp.roomDB.Category
+import com.viz.to_do_listapp.screens.AddCategory
+import com.viz.to_do_listapp.screens.AddTask
 import com.viz.to_do_listapp.screens.Calendar
 import com.viz.to_do_listapp.screens.HomePage
-import com.viz.to_do_listapp.screens.Task
 import com.viz.to_do_listapp.viewModel.TaskViewModel
 
 @Composable
-fun Navigation(viewModel: TaskViewModel) {
+fun Navigation(viewModel: TaskViewModel, darkTheme : Boolean, onThemeUpdated : () -> Unit) {
     val navController = rememberNavController()
+    var categoryList by remember { mutableStateOf(listOf<Category>()) }
+    viewModel.getCategories().observe(LocalLifecycleOwner.current) { categoryList = it }
 
     NavHost(
         navController = navController,
         startDestination = Routes.App
     ) {
         composable(Routes.App) {
-            App(viewModel,navController = navController)
+            App(viewModel,navController = navController, darkTheme, onThemeUpdated)
         }
         composable(Routes.Home.route) {
-            HomePage(viewModel, navController = navController)
+            HomePage(viewModel, darkTheme) //, navController = navController
         }
         composable(Routes.Calendar.route) {
-            Calendar(navController = navController)
+            Calendar() //navController = navController
         }
         composable(Routes.Task) {
-            Task(viewModel, navController = navController)
+            AddTask(viewModel, navController = navController, categoryList)
+        }
+        composable(Routes.category) {
+            AddCategory(viewModel = viewModel, navController)
         }
 
     }
