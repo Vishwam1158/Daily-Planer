8de96699686c8381c8f0b152954dcf40fd760637
Vishwam1158 <vishwam1158@gmail.com>
2024-07-05 23:54:49 +0530

    add kleeOne font

--- app/src/main/java/com/viz/to_do_listapp/ui/theme/Type.kt
+++ app/src/main/java/com/viz/to_do_listapp/ui/theme/Type.kt
@@ -2,14 +2,22 @@ package com.viz.to_do_listapp.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.viz.to_do_listapp.R
 
 // Set of Material typography styles to start with
+
+val font_primary = FontFamily(
+    Font(R.font.klee_one),
+
+)
+
 val Typography = Typography(
     bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = font_primary,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp,
         lineHeight = 24.sp,
