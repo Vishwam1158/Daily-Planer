b836580fcbe3361709c8fce8bca6b449161f28aa
Vishwam1158 <vishwam1158@gmail.com>
2024-07-05 23:55:09 +0530

    version update

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,14 +4,14 @@ plugins {
     alias(libs.plugins.jetbrains.kotlin.android) apply false
 }
 
-buildscript {
-
-    // The following section is needed only if pluginMangement is not used in settings.gradle
-    repositories {
-        mavenCentral()
-    }
-
-    dependencies {
-        classpath("com.newrelic.agent.android:agent-gradle-plugin:7.4.1")
-    }
-}
+//buildscript {
+//
+//    // The following section is needed only if pluginMangement is not used in settings.gradle
+//    repositories {
+//        mavenCentral()
+//    }
+//
+//    dependencies {
+//        classpath("com.newrelic.agent.android:agent-gradle-plugin:7.4.1")
+//    }
+//}
