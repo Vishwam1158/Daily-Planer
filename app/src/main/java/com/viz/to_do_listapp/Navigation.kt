bb0c98da541a702d04ed9978e46d62d8c95b03de
Vishwam1158 <vishwam1158@gmail.com>
2024-07-08 22:41:55 +0530

    Remove Calendar and Category Routes

--- app/src/main/java/com/viz/to_do_listapp/Navigation.kt
+++ app/src/main/java/com/viz/to_do_listapp/Navigation.kt
@@ -9,10 +9,8 @@ import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import com.viz.to_do_listapp.roomDB.Category
-import com.viz.to_do_listapp.screens.AddCategory
+import com.viz.to_do_listapp.Model.Category
 import com.viz.to_do_listapp.screens.AddTask
-import com.viz.to_do_listapp.screens.Calendar
 import com.viz.to_do_listapp.screens.HomePage
 import com.viz.to_do_listapp.viewModel.TaskViewModel
 
@@ -32,15 +30,8 @@ fun Navigation(viewModel: TaskViewModel, darkTheme : Boolean, onThemeUpdated : (
         composable(Routes.Home.route) {
             HomePage(viewModel, darkTheme) //, navController = navController
         }
-        composable(Routes.Calendar.route) {
-            Calendar() //navController = navController
-        }
         composable(Routes.Task) {
             AddTask(viewModel, navController = navController, categoryList)
         }
-        composable(Routes.category) {
-            AddCategory(viewModel = viewModel, navController)
-        }
-
     }
 }
