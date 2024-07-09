384d04e2c1a47a9b3532867741c9eb82ec358002
Vishwam1158 <vishwam1158@gmail.com>
2024-07-09 23:20:19 +0530

    Add Queries

--- app/src/main/java/com/viz/to_do_listapp/roomDB/RoomDao.kt
+++ app/src/main/java/com/viz/to_do_listapp/roomDB/RoomDao.kt
@@ -1,9 +1,12 @@
 package com.viz.to_do_listapp.roomDB
 
+import androidx.lifecycle.LiveData
 import androidx.room.Dao
 import androidx.room.Delete
 import androidx.room.Query
 import androidx.room.Upsert
+import com.viz.to_do_listapp.Model.Category
+import com.viz.to_do_listapp.Model.Task
 import kotlinx.coroutines.flow.Flow
 
 @Dao
@@ -18,12 +21,21 @@ interface RoomDao {
     @Delete
     suspend fun deleteCategory(category: Category)
 
-    @Query("SELECT * FROM task")
+    @Query("SELECT * FROM task ORDER BY id ASC")
     fun getAllTasks(): Flow<List<Task>>
     @Query("SELECT * FROM category")
     fun getAllCategories(): Flow<List<Category>>
     @Query("SELECT * FROM task WHERE categoryId = :categoryId")
     fun getTasksByCategory(categoryId: Int): Flow<List<Task>>
+    @Query("SELECT * FROM task WHERE title LIKE :searchQuery")
+    fun searchDatabase(searchQuery: String): LiveData<List<Task>>
+    @Query("SELECT * FROM task ORDER BY CASE WHEN priority LIKE 'H%' THEN 1 WHEN priority LIKE 'M%' THEN 2 WHEN priority LIKE 'L%' THEN 3 END")
+    fun sortByPriority(): LiveData<List<Task>>
+    @Query("SELECT * FROM task WHERE isComplete = 'true' ")
+    fun getCompleteTasks(): LiveData<List<Task>>
+    @Query("SELECT * FROM task WHERE isComplete = 'false' ")
+    fun getIncompleteTasks(): LiveData<List<Task>>
 
-
+//    @Query("SELECT * FROM task ORDER BY CASE WHEN priority LIKE 'L%' THEN 1 WHEN priority LIKE 'M%' THEN 2 WHEN priority LIKE 'H%' THEN 3 END")
+//    fun sortByLowPriority(): LiveData<List<Task>>
 }
