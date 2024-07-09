11c15cb88a1f706f23125aa39855d8104a2d0870
Vishwam1158 <vishwam1158@gmail.com>
2024-07-09 23:19:41 +0530

    Auto select new Added Category

--- app/src/main/java/com/viz/to_do_listapp/screens/Task.kt
+++ app/src/main/java/com/viz/to_do_listapp/screens/Task.kt
@@ -1,6 +1,5 @@
 package com.viz.to_do_listapp.screens
 
-import android.graphics.Outline
 import android.widget.Toast
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
@@ -12,27 +11,22 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AddCircleOutline
 import androidx.compose.material.icons.filled.ArrowDropDown
 import androidx.compose.material.icons.filled.Check
-import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.DropdownMenuItem
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
-import androidx.compose.material3.LocalTextStyle
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
@@ -46,21 +40,18 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Color.Companion.Transparent
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
-import androidx.navigation.compose.rememberNavController
 import com.viz.to_do_listapp.Routes
-import com.viz.to_do_listapp.Routes.category
-import com.viz.to_do_listapp.roomDB.Category
+import com.viz.to_do_listapp.Model.Category
 import com.viz.to_do_listapp.viewModel.TaskViewModel
-import com.viz.to_do_listapp.roomDB.Task
+import com.viz.to_do_listapp.Model.Task
 
 
-//MAin Add Task
 @Composable
 fun AddTask(viewModel: TaskViewModel, navController: NavController, categories : List<Category>) {
     var title by remember { mutableStateOf("") } // don't put " " always put "" as value, try it and placeholder boom!!
@@ -69,17 +60,21 @@ fun AddTask(viewModel: TaskViewModel, navController: NavController, categories :
 
     var selectedCategoryId by remember { mutableStateOf(1) }
     var selectedCategoryName by remember { mutableStateOf("Category") }
-    val task = Task(title, description, categoryId = selectedCategoryId)
     var isExpanded by remember { mutableStateOf(false) }
+    var showNewCategoryField by remember { mutableStateOf(false) }
+    var newCategoryName by remember { mutableStateOf("") }
+
+    val task = Task(title, description, categoryId = selectedCategoryId)
 
 //    var status by remember { mutableStateOf(showDialogBox) }
 
     Column(
         modifier = Modifier
             .fillMaxSize()
-            .padding(start = 24.dp, end = 24.dp, top = 36.dp, bottom = 24.dp)
+            .padding(start = 24.dp, end = 24.dp)//, top = 56.dp, bottom = 24.dp)
             .background(MaterialTheme.colorScheme.background),
-        horizontalAlignment = Alignment.CenterHorizontally
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
     ) {
 
         TextField(
@@ -98,11 +93,12 @@ fun AddTask(viewModel: TaskViewModel, navController: NavController, categories :
             placeholder = {
                 Text(
                     text = "Add Task",
-                    color = MaterialTheme.colorScheme.onBackground,
+                    color = Color.Gray,
                     fontWeight = FontWeight(600),
                     fontSize = 24.sp
                 )
             },
+            textStyle = TextStyle(fontSize = 24.sp, fontWeight = FontWeight.SemiBold,),
             colors = TextFieldDefaults.colors(
                 focusedTextColor = MaterialTheme.colorScheme.onBackground,
                 unfocusedTextColor = MaterialTheme.colorScheme.onBackground,
@@ -126,12 +122,13 @@ fun AddTask(viewModel: TaskViewModel, navController: NavController, categories :
             },
             placeholder = {
                 Text(
-                    text = "Add description (optional)",
-                    color = MaterialTheme.colorScheme.onBackground,
+                    text = "Add description ?",
+                    color = Color.Gray,
                     fontWeight = FontWeight(600),
-                    fontSize = 24.sp
+                    fontSize = 20.sp
                 )
             },
+            textStyle = TextStyle(fontSize = 20.sp, fontWeight = FontWeight.Normal),
             colors = TextFieldDefaults.colors(
                 focusedTextColor = MaterialTheme.colorScheme.onBackground,
                 unfocusedTextColor = MaterialTheme.colorScheme.onBackground,
@@ -148,59 +145,139 @@ fun AddTask(viewModel: TaskViewModel, navController: NavController, categories :
         )
         Spacer(modifier = Modifier.padding(12.dp))
 
+        // Category Drop Down Menu and + Icon
         Row(
-            horizontalArrangement = Arrangement.SpaceBetween,
+            horizontalArrangement = Arrangement.Center,
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier.fillMaxWidth()
         ) {
 
+            Text(
+                text = "Add to :",
+                color = MaterialTheme.colorScheme.onBackground,
+                fontWeight = FontWeight(600),
+                fontSize = 18.sp
+            )
             Button(
                 onClick = { isExpanded = !isExpanded },
                 shape = RoundedCornerShape(12),
                 colors = ButtonDefaults.buttonColors(Color.Transparent),
-                border = BorderStroke(1.dp, Color.Transparent)
+                border = BorderStroke(1.dp, Color.Transparent),
             ) {
                 Text(
                     text = selectedCategoryName,
                     color = MaterialTheme.colorScheme.onBackground
                 )
-
+                Spacer(modifier = Modifier.width(8.dp))
                 Icon(
                     imageVector = Icons.Filled.ArrowDropDown,
-                    contentDescription = "Drop Down Arrow"
+                    contentDescription = "Drop Down Arrow",
+                    tint = MaterialTheme.colorScheme.onBackground
                 )
             }
 
-            DropdownMenu(
-                expanded = isExpanded,
-                onDismissRequest = {
-                    isExpanded = false
-                },
-            ) {
-                categories.forEach { category ->
-                    DropdownMenuItem(
-                        text = { Text(text = category.name) },
-                        onClick = {
-                            selectedCategoryId = category.id
-                            selectedCategoryName = category.name
-                            isExpanded = false
-                        },
-                        modifier = Modifier
-                            .border(
-                                1.dp, MaterialTheme.colorScheme.onBackground
-                            )
-                            .align(Alignment.End)
-                    )
+
+                DropdownMenu(
+                    expanded = isExpanded,
+                    onDismissRequest = {
+                        isExpanded = false
+                    },
+                    modifier = Modifier.padding(4.dp)
+                ) {
+                    categories.forEach { category ->
+                        DropdownMenuItem(
+                            text = { Text(text = category.name) },
+                            onClick = {
+                                selectedCategoryId = category.id
+                                selectedCategoryName = category.name
+                                isExpanded = false
+                            },
+                            modifier = Modifier.align(Alignment.CenterHorizontally)
+//                            .border(
+//                                1.dp, MaterialTheme.colorScheme.onBackground
+//                            )
+//                            .align(Alignment.End)
+                        )
+                    }
                 }
-            }
+
 
             IconButton(
                 onClick = {
-                    navController.navigate(Routes.category)
+                    showNewCategoryField = !showNewCategoryField
                 }
             ) {
                 Icon(
                     imageVector = Icons.Default.AddCircleOutline,
-                    contentDescription = "Add Category"
+                    contentDescription = "Add Category",
+                    tint = MaterialTheme.colorScheme.onBackground
                 )
+            }
+        }
+
+        // New Category Text Field
+        if (showNewCategoryField) {
+            Spacer(modifier = Modifier.padding(8.dp))
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.SpaceBetween,
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                TextField(
+                    value = newCategoryName,
+                    onValueChange = {
+                        if (it.length <= 15) {
+                            newCategoryName = it
+                        } else {
+                            Toast.makeText(context, "Category cannot be more than 15 characters", Toast.LENGTH_SHORT).show()
+                        }
+                    },
+                    placeholder = {
+                        Text(
+                            text = "New Category",
+                            color = Color.Gray,
+                            fontWeight = FontWeight(600),
+                            fontSize = 20.sp
+                        )
+                    },
+                    textStyle = TextStyle(fontSize = 20.sp, fontWeight = FontWeight.Normal),
+                    colors = TextFieldDefaults.colors(
+                        focusedTextColor = MaterialTheme.colorScheme.onBackground,
+                        unfocusedTextColor = MaterialTheme.colorScheme.onBackground,
+                        focusedIndicatorColor = MaterialTheme.colorScheme.onBackground, // Underline Color
+                        cursorColor = MaterialTheme.colorScheme.onBackground,
+                        focusedContainerColor = Transparent,  // Background color
+                        unfocusedContainerColor = Transparent,
+                    ),
+                    modifier = Modifier
+                        .weight(1f)
+                        .background(Color.Transparent),
+                    keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Text),
+                    singleLine = true
+                )
+                IconButton(
+                    onClick = {
+                        if (newCategoryName.isNotBlank()) {
+                            viewModel.upsertCategory(Category(name = newCategoryName))
+                            selectedCategoryName = newCategoryName
+                            selectedCategoryId = categories.last().id+1 //auto added Task in the new Created Category
+//                            newCategoryName = " "
+                            showNewCategoryField = false
+                        } else {
+                            Toast.makeText(
+                                context,
+                                "Category name cannot be empty",
+                                Toast.LENGTH_SHORT
+                            ).show()
+                        }
+                    }
+                ) {
+                    Icon(
+                        imageVector = Icons.Filled.Check,
+                        contentDescription = "Add New Category",
+                        tint = MaterialTheme.colorScheme.onBackground
+                    )
+                }
             }
         }
     }
@@ -211,13 +288,11 @@ fun AddTask(viewModel: TaskViewModel, navController: NavController, categories :
         verticalAlignment = Alignment.Bottom
     ) {
             CustomFloatingActionButton(
-                navController = navController,
                 onClick = {
                     if (task.title.isNotBlank()) {
                         viewModel.upsertTask(task)
                         navController.navigate(Routes.App)
-                    } else Toast.makeText(context, "Please enter a title", Toast.LENGTH_SHORT)
-                        .show()
+                    } else navController.popBackStack()
                 },
                 icon = Icons.Filled.Check
             )
