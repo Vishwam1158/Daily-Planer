bc91a722d9b0fe4715014120caefed4ed6b06c16
Vishwam1158 <vishwam1158@gmail.com>
2024-07-09 23:19:22 +0530

    Add priority

--- app/src/main/java/com/viz/to_do_listapp/Model/Task.kt
+++ app/src/main/java/com/viz/to_do_listapp/Model/Task.kt
@@ -1,4 +1,4 @@
-package com.viz.to_do_listapp.roomDB
+package com.viz.to_do_listapp.Model
 
 import androidx.room.Entity
 import androidx.room.PrimaryKey
@@ -9,6 +9,7 @@ data class Task (
     val description: String,
     val isComplete: Boolean = false,
     val categoryId: Int,
+    var priority: Priority = Priority.MEDIUM,
     @PrimaryKey(autoGenerate = true)
     val id: Int = 0
 )
