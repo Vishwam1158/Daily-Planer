f8b5bfd83720bebda8cc51a31a8efa9ad4b5c3df
Vishwam1158 <vishwam1158@gmail.com>
2024-07-12 22:35:16 +0530

    Filter option Added

--- app/src/main/java/com/viz/to_do_listapp/viewModel/TaskViewModel.kt
+++ app/src/main/java/com/viz/to_do_listapp/viewModel/TaskViewModel.kt
@@ -1,25 +1,34 @@
 package com.viz.to_do_listapp.viewModel
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.asLiveData
 import androidx.lifecycle.viewModelScope
-import com.viz.to_do_listapp.roomDB.Category
-import com.viz.to_do_listapp.roomDB.Task
+import com.viz.to_do_listapp.Model.Category
+import com.viz.to_do_listapp.Model.Task
+import com.viz.to_do_listapp.screens.FilterOption
 import kotlinx.coroutines.launch
 
-class TaskViewModel(private val repository: Repository): ViewModel() {
+class TaskViewModel(private val repository: Repository) : ViewModel() {
+    private val _filterOption = MutableLiveData<FilterOption>(FilterOption.Alphabet)
+    val filterOption: LiveData<FilterOption> get() = _filterOption
+
     fun getTasks() = repository.getAllTasks().asLiveData(viewModelScope.coroutineContext)
     fun getCategories() = repository.getAllCategories().asLiveData(viewModelScope.coroutineContext)
     fun getTasksByCategory(categoryId: Int) = repository.getTasksByCategory(categoryId).asLiveData(viewModelScope.coroutineContext)
 
+    fun applyFilter(filterOption: FilterOption) {
+        _filterOption.value = filterOption
+    }
 
-    fun upsertTask(task: Task){
+    fun upsertTask(task: Task) {
         viewModelScope.launch {
-                repository.upsertTask(task)
+            repository.upsertTask(task)
         }
     }
 
-    fun deleteTask(task: Task){
+    fun deleteTask(task: Task) {
         viewModelScope.launch {
             repository.deleteTask(task)
         }
@@ -30,8 +39,7 @@ class TaskViewModel(private val repository: Repository): ViewModel() {
         upsertTask(updatedTask)
     }
 
-
-    fun upsertCategory(category: Category){
+    fun upsertCategory(category: Category) {
         viewModelScope.launch {
             repository.upsertCategory(category)
         }
