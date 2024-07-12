aaf3a8888125673417b72efe29fa0c122855412e
Vishwam1158 <vishwam1158@gmail.com>
2024-07-12 23:03:30 +0530

    fix Serch Button

--- app/src/main/java/com/viz/to_do_listapp/App.kt
+++ app/src/main/java/com/viz/to_do_listapp/App.kt
@@ -35,7 +35,7 @@ fun App(
                     viewModel.applyFilter(filterOption)
                 },
                 onSearchClick = {
-                    showSearch = true
+                    showSearch = !showSearch
                 }
             )
         },
